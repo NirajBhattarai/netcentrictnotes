fad26aff514c30ac0e6f55add4939535bf17e160
Niraj Bhattarai <39485271+NirajBhattarai@users.noreply.github.com>
2023-04-25 08:04:00 +0545

    added private access modifier

---
+++ programs/unit1/accessmodifiers/private/privateaccessmodifier/privateaccessmodifier/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApplication
+{
+
+    class Student
+    {
+        private string name = "Sheeran";
+
+        private void print()
+        {
+            Console.WriteLine("Hello from Student class");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // creating object of Student class
+            Student student1 = new Student();
+
+            // accessing name field and printing it
+            Console.WriteLine("Name: " + student1.name);
+
+            // accessing print method from Student
+            student1.print();
+
+            Console.ReadLine();
+        }
+    }
+}
