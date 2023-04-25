6b6e7c0bfc616f6ac6e2c9b9789b52147a56e672
Niraj Bhattarai <39485271+NirajBhattarai@users.noreply.github.com>
2023-04-25 08:00:21 +0545

    added public access specifier

---
+++ programs/unit1/accessmodifiers/public/publicaccessmodifier/publicaccessmodifier/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+//using publicaccessmodifier.newnamespace; explicit import
+
+namespace MyApplication
+{
+
+    class Student
+    {
+        public string name = "Sheeran";
+
+        public void print()
+        {
+            Console.WriteLine("Hello from Student class");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // creating object of Student class
+            Student student1 = new Student();
+
+            // accessing name field and printing it
+            Console.WriteLine("Name: " + student1.name);
+
+            // accessing print method from Student
+            student1.print();
+            Console.ReadLine();
+
+            publicaccessmodifier.newnamespace.College college = new publicaccessmodifier.newnamespace.College();
+
+            Console.WriteLine(college.name);
+        }
+    }
+}
---
+++ programs/unit1/accessmodifiers/public/publicaccessmodifier/publicaccessmodifier/newnamespace/College.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace publicaccessmodifier.newnamespace
+{
+    public class College
+    {
+        public string name = "Nepathya";
+        public College()
+        {
+        }
+    }
+}
+
