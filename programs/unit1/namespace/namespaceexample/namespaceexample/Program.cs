009e86b0bcdc773cb3411db682d0c6cb97acdc46
Niraj Bhattarai <39485271+NirajBhattarai@users.noreply.github.com>
2023-04-25 07:31:07 +0545

    added example for namespace
    added example for namespace

---
+++ programs/unit1/namespace/namespaceexample/namespaceexample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using namespaceexample.model;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            st.firstname = "sagara";
+            st.lastname = "xyz";
+            st.rollno = 10;
+            st.address = "manigram";
+            Console.WriteLine(st.firstname);
+            Console.WriteLine(st.lastname);
+            Console.Write(st.rollno);
+        }
+    }
+}
---
+++ programs/unit1/namespace/namespaceexample/namespaceexample/model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace namespaceexample.model
+{
+    public class Student
+    {
+        public string firstname;
+        public string lastname;
+        public string address;
+        public int rollno;
+        public Student()
+        {
+        }
+    }
+}
+
