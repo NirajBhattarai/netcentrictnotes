3bfe0e603c14c88950db85978d2e058b800d0d87
Niraj Bhattarai <39485271+NirajBhattarai@users.noreply.github.com>
2023-05-07 22:57:58 +0545

    changed location

---
+++ programs/unit1/FileIO/FileIO/FileIO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+
+class FileIOExample
+{
+    static void Main()
+    {
+        string inputFilePath = "input.txt";
+        string outputFilePath = "output.txt";
+
+        try
+        {
+            // Check if the input file exists
+            if (File.Exists(inputFilePath))
+            {
+                // Read all lines from the input file
+                string[] lines = File.ReadAllLines(inputFilePath);
+
+                // Process the lines and store them in a new array
+                string[] processedLines = new string[lines.Length];
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    processedLines[i] = $"Line {i + 1}: {lines[i]}";
+                }
+
+                // Write the processed lines to the output file
+                File.WriteAllLines(outputFilePath, processedLines);
+
+                Console.WriteLine("File processing completed. Processed content written to 'output.txt'.");
+            }
+            else
+            {
+                Console.WriteLine("Input file not found.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred while processing the file:");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
